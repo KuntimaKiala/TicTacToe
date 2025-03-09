8b4a8475502bda41d2cde1d99a44f3a40554ab72
kuntimakiala <kuntimakiala@github.com>
2025-03-09 01:28:30 +0100

    Window Creator

---
+++ Engine/include/engine/WindowManager.h
@@ -0,0 +1,32 @@
+#pragma once 
+#include "glad/glad.h"
+#include "GLFW/glfw3.h"
+
+
+namespace FromHeLL
+{
+    
+    class WindowManager
+    {
+    private:
+        int m_iWidth; 
+        int m_iHeight; 
+        const char* m_pAppName;
+        GLFWwindow* m_pWindow;
+        
+        static void framebuffer_size_callback(GLFWwindow*, int iWidth, int iHeight) ;
+        bool IsGladSucceded() ;
+        void ResizeWindow();
+
+    public:
+        WindowManager() = delete;
+        WindowManager(int iWidth, int iHeight, const char* pAppName);
+        inline GLFWwindow* GetWindow() {return m_pWindow;}
+        bool IsWindowCreated() ;
+        
+        void Init() ;
+        ~WindowManager();
+    };
+    
+    
+} // namespace FromHeLL
