7550741dc2063c64301fad0e226d71e88a1092bd
kuntimakiala <kuntimakiala@github.com>
2025-03-09 08:37:24 +0100

    Display Background Color

--- Game/src/game/GameApplication.cpp
+++ Game/src/game/GameApplication.cpp
@@ -19,12 +19,12 @@ namespace FromHeLL
         std::cout << "Game Running" <<std::endl;
         while (!glfwWindowShouldClose( GetWindowManager()->GetWindow() ))
         {
-            /*
+            
                 glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
                 glClear(GL_COLOR_BUFFER_BIT);
                 glfwSwapBuffers(GetWindowManager()->GetWindow());
                 glfwPollEvents();
-            */
+            
 
         }
         
